cb7ca2c11d820402624881755882cf23fec96dc9
flynndi <lixuan0520@gmail.com>
2024-02-28 17:10:52 +0800

    Interceptor example

---
+++ integration-tests/src/main/java/io/quarkiverse/jimmer/it/config/BaseEntityDraftInterceptor.java
@@ -0,0 +1,24 @@
+package io.quarkiverse.jimmer.it.config;
+
+import io.quarkiverse.jimmer.it.entity.BaseEntity;
+import io.quarkiverse.jimmer.it.entity.BaseEntityDraft;
+import io.quarkiverse.jimmer.it.entity.BaseEntityProps;
+import io.quarkus.arc.Unremovable;
+import jakarta.inject.Singleton;
+import org.babyfish.jimmer.ImmutableObjects;
+import org.babyfish.jimmer.sql.DraftInterceptor;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.LocalDateTime;
+
+@Singleton
+@Unremovable
+public class BaseEntityDraftInterceptor implements DraftInterceptor<BaseEntity, BaseEntityDraft> {
+
+    @Override
+    public void beforeSave(BaseEntityDraft draft, @Nullable BaseEntity original) {
+        if (!ImmutableObjects.isLoaded(draft, BaseEntityProps.MODIFIED_TIME)) {
+            draft.setModifiedTime(LocalDateTime.now());
+        }
+    }
+}
