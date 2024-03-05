7e5f3bd893bfa93eb8f03b1ad10943201a3837a4
flynndi <lixuan0520@gmail.com>
2024-03-05 20:00:46 +0800

    Page integration-tests

--- integration-tests/src/test/java/io/quarkiverse/jimmer/it/resource/TestResourceTestCase.java
+++ integration-tests/src/test/java/io/quarkiverse/jimmer/it/resource/TestResourceTestCase.java
@@ -1,15 +1,28 @@
 package io.quarkiverse.jimmer.it.resource;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.post;
+
+import jakarta.inject.Inject;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import io.netty.handler.codec.http.HttpHeaderValues;
 import io.quarkiverse.jimmer.it.repository.BookRepository;
 import io.quarkiverse.jimmer.it.repository.BookStoreRepository;
 import io.quarkus.arc.Arc;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.Header;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.vertx.core.http.HttpHeaders;
 
 @QuarkusTest
 public class TestResourceTestCase {
+
+    @Inject
+    BookRepository bookRepository;
 
     @Test
     void testRepository() {
@@ -17,5 +30,25 @@ public class TestResourceTestCase {
         BookStoreRepository bookStoreRepository = Arc.container().instance(BookStoreRepository.class).get();
         Assertions.assertNotNull(bookRepository);
         Assertions.assertNotNull(bookStoreRepository);
+    }
+
+    @Test
+    void testPage() {
+        String body = """
+                {
+                    "index": 0,
+                    "size": 1
+                }
+                """;
+        Response post = given()
+                .body(body)
+                .header(new Header(HttpHeaders.CONTENT_TYPE.toString(), HttpHeaderValues.APPLICATION_JSON.toString()))
+                .log()
+                .all()
+                .when()
+                .post("testResources/testBookRepositoryPage");
+        JsonPath responseJsonPath = post.jsonPath();
+        Assertions.assertEquals(6, responseJsonPath.getInt("totalRowCount"));
+        Assertions.assertEquals(6, responseJsonPath.getInt("totalPageCount"));
     }
 }
