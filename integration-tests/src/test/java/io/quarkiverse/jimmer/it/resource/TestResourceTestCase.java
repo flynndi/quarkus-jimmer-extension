cc21e9eaa8c47fe821c03ba7ac74b5c75cb9f775
flynndi <lixuan0520@gmail.com>
2024-03-09 17:46:44 +0800

    Integration-tests

--- integration-tests/src/test/java/io/quarkiverse/jimmer/it/resource/TestResourceTestCase.java
+++ integration-tests/src/test/java/io/quarkiverse/jimmer/it/resource/TestResourceTestCase.java
@@ -139,6 +139,28 @@ public class TestResourceTestCase {
     }
 
     @Test
+    void testBookRepositoryByIdOptionalPresent() {
+        Response response = given()
+                .queryParam("id", 1L)
+                .log()
+                .all()
+                .when()
+                .get("testResources/testBookRepositoryByIdOptional");
+        Assertions.assertNotNull(response.jsonPath());
+    }
+
+    @Test
+    void testBookRepositoryByIdOptionalEmpty() {
+        Response response = given()
+                .queryParam("id", 0)
+                .log()
+                .all()
+                .when()
+                .get("testResources/testBookRepositoryByIdOptional");
+        Assertions.assertEquals(HttpStatus.SC_NO_CONTENT, response.statusCode());
+    }
+
+    @Test
     void testBookRepositoryByIdFetcher() {
         Response response = given()
                 .queryParam("id", 0)
@@ -147,6 +169,28 @@ public class TestResourceTestCase {
                 .when()
                 .get("testResources/testBookRepositoryByIdFetcher");
         Assertions.assertEquals(response.statusCode(), HttpStatus.SC_NO_CONTENT);
+    }
+
+    @Test
+    void testBookRepositoryByIdFetcherOptionalPresent() {
+        Response response = given()
+                .queryParam("id", 1L)
+                .log()
+                .all()
+                .when()
+                .get("testResources/testBookRepositoryByIdFetcherOptional");
+        Assertions.assertNotNull(response.jsonPath());
+    }
+
+    @Test
+    void testBookRepositoryByIdFetcherOptionalEmpty() {
+        Response response = given()
+                .queryParam("id", 0)
+                .log()
+                .all()
+                .when()
+                .get("testResources/testBookRepositoryByIdFetcherOptional");
+        Assertions.assertEquals(response.body().print(), "");
     }
 
     @Test
@@ -161,6 +205,22 @@ public class TestResourceTestCase {
         Assertions.assertEquals(1, response.jsonPath().getLong("id"));
         Assertions.assertNotNull(response.jsonPath().getJsonObject("store"));
         Assertions.assertNotNull(response.jsonPath().getJsonObject("authors"));
+    }
+
+    @Test
+    void testBookRepositoryFindAllById() {
+        String body = """
+                [1, 2]
+                """;
+        Response response = given()
+                .header(new Header(HttpHeaders.CONTENT_TYPE.toString(), HttpHeaderValues.APPLICATION_JSON.toString()))
+                .body(body)
+                .log()
+                .all()
+                .when()
+                .post("testResources/testBookRepositoryFindAllById");
+        Assertions.assertNotNull(response.jsonPath());
+        Assertions.assertEquals(1, response.jsonPath().getLong("[0].id"));
     }
 
     @Test
