093a83f8cd54aae9f1b45c3986baf65396aa0805
flynndi <lixuan0520@gmail.com>
2024-04-03 08:14:56 +0800

    rename

--- runtime/src/main/java/io/quarkiverse/jimmer/runtime/QuarkusSqlClientProducer.java
+++ runtime/src/main/java/io/quarkiverse/jimmer/runtime/QuarkusSqlClientProducer.java
@@ -17,7 +17,7 @@ import io.quarkus.arc.ArcContainer;
 /**
  * This class is sort of a producer for {@link JQuarkusSqlClient}.
  * It isn't a CDI producer in the literal sense, but it is marked as a bean
- * and it's {@code createJQuarkusSqlClient} or {@code createKQuarkusSqlClient}
+ * and it's {@code createQuarkusJSqlClient} or {@code createQuarkusKSqlClient}
  * method is called at runtime in order to produce
  * the actual {@code JSqlClient} or {@code KSqlClient} objects.
  * CDI scopes and qualifiers are set up at build-time, which is why this class is devoid of
@@ -38,14 +38,14 @@ public class QuarkusSqlClientProducer {
         this.event = event;
     }
 
-    public QuarkusJSqlClientContainer createJQuarkusSqlClient(DataSource dataSource, String dataSourceName, Dialect dialect) {
+    public QuarkusJSqlClientContainer createQuarkusJSqlClient(DataSource dataSource, String dataSourceName, Dialect dialect) {
         final JSqlClient jSqlClient = SqlClients.java(config, dataSource, dataSourceName, container, null,
                 event,
                 dialect);
         return new QuarkusJSqlClientContainer(jSqlClient, dataSourceName);
     }
 
-    public QuarkusKSqlClientContainer createKQuarkusSqlClient(DataSource dataSource, String dataSourceName, Dialect dialect) {
+    public QuarkusKSqlClientContainer createQuarkusKSqlClient(DataSource dataSource, String dataSourceName, Dialect dialect) {
         final KSqlClient kSqlClient = SqlClients.kotlin(config, dataSource, dataSourceName, container, null, event, dialect);
         return new QuarkusKSqlClientContainer(kSqlClient, dataSourceName);
     }
