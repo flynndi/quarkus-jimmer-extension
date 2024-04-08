89ca08d1bcaeb068e22c3e1232db78a33a035740
flynndi <lixuan0520@gmail.com>
2024-04-08 08:21:19 +0800

    Integration-tests

--- integration-tests/src/main/java/io/quarkiverse/jimmer/it/entity/BaseEntity.java
+++ integration-tests/src/main/java/io/quarkiverse/jimmer/it/entity/BaseEntity.java
@@ -4,14 +4,10 @@ import java.time.LocalDateTime;
 
 import org.babyfish.jimmer.sql.MappedSuperclass;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 @MappedSuperclass
 public interface BaseEntity {
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime createdTime();
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime modifiedTime();
 }
