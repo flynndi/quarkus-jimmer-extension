34efeaccf06898a6680fa9395f91293388cc15a9
flynndi <lixuan0520@gmail.com>
2024-04-19 08:14:56 +0800

    Integration-tests

--- integration-tests/src/main/resources/h2-database2.sql
+++ integration-tests/src/main/resources/h2-database2.sql
@@ -1,13 +1,15 @@
-CREATE TABLE public.user_role (
-    id varchar(64) NOT NULL,
-    user_id varchar(64) NULL,
-    role_id varchar(64) NULL,
-    delete_flag bool NOT NULL DEFAULT false,
-    auth_user varchar(64) NULL,
-    CONSTRAINT user_role_pkey PRIMARY KEY (id)
+drop table public.user_role if exists;
+
+create table public.user_role (
+    id varchar(64) not null,
+    user_id varchar(64) null,
+    role_id varchar(64) null,
+    delete_flag bool not null default false,
+    auth_user varchar(64) null,
+    constraint user_role_pkey primary key (id)
 );
 
 
-INSERT INTO public.user_role
+insert into public.user_role
 (id, user_id, role_id, delete_flag, auth_user)
-VALUES('defc2d01-fb38-4d31-b006-fd182b25aa33', '9ffec3c4-2342-427c-a0ec-e22e5f2ec732', '2c6a06d8-8e10-49c4-88fe-7d2f05dd073b', false, '{"id":"123"}');
+values('defc2d01-fb38-4d31-b006-fd182b25aa33', '9ffec3c4-2342-427c-a0ec-e22e5f2ec732', '2c6a06d8-8e10-49c4-88fe-7d2f05dd073b', false, '{"id":"123"}');
