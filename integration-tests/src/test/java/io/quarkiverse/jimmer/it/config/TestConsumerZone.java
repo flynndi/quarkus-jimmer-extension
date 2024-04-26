acf19676b8d3ec93b021987564c3c603e41ac263
flynndi <lixuan0520@gmail.com>
2024-04-26 08:21:41 +0800

    Integration-tests

--- integration-tests/src/test/java/io/quarkiverse/jimmer/it/config/TestConsumerZone.java
+++ integration-tests/src/test/java/io/quarkiverse/jimmer/it/config/TestConsumerZone.java
@@ -11,6 +11,7 @@ import org.babyfish.jimmer.sql.runtime.JSqlClientImplementor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import io.quarkiverse.jimmer.it.Constant;
 import io.quarkiverse.jimmer.runtime.Jimmer;
 import io.quarkus.arc.Unremovable;
 import io.quarkus.test.junit.QuarkusTest;
@@ -22,7 +23,12 @@ public class TestConsumerZone {
     void testConsumer() {
         JSqlClientImplementor defaultJSqlClientImplementor = (JSqlClientImplementor) Jimmer.getDefaultJSqlClient();
         ZoneId zoneId = defaultJSqlClientImplementor.getZoneId();
+
+        JSqlClientImplementor jSqlClientImplementorDB2 = (JSqlClientImplementor) Jimmer.getJSqlClient(Constant.DATASOURCE2);
+        ZoneId zoneIdDB2 = jSqlClientImplementorDB2.getZoneId();
+
         Assertions.assertEquals(zoneId, ZoneId.of("Asia/Chongqing"));
+        Assertions.assertEquals(zoneIdDB2, ZoneId.of("Asia/Chongqing"));
     }
 
     @ApplicationScoped
