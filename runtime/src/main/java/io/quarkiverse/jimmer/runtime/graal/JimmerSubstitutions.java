a9574c6ffc78e3e00cd8b1c8de9340e1adbe24d2
flynndi <lixuan0520@gmail.com>
2024-05-18 15:34:13 +0800

    Graalvm config

--- runtime/src/main/java/io/quarkiverse/jimmer/runtime/graal/JimmerSubstitutions.java
+++ runtime/src/main/java/io/quarkiverse/jimmer/runtime/graal/JimmerSubstitutions.java
@@ -6,7 +6,6 @@ import org.babyfish.jimmer.sql.runtime.Reader;
 
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.oracle.svm.core.annotate.Delete;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 
@@ -36,18 +35,13 @@ public class JimmerSubstitutions {
         }
     }
 
-    @TargetClass(className = "org.babyfish.jimmer.sql.dialect.PostgresDialect", onlyWith = JimmerSubstitutions.IsPGAbsent.class)
-    @Delete
-    public static final class PostgresDialectDelete {
-    }
-
     public static final class IsPGAbsent implements BooleanSupplier {
 
         private boolean pgAbsent;
 
         public IsPGAbsent() {
             try {
-                Class.forName("org.postgresql.Driver");
+                Class.forName("org.postgresql.Driver", true, Thread.currentThread().getContextClassLoader());
                 pgAbsent = false;
             } catch (ClassNotFoundException e) {
                 pgAbsent = true;
