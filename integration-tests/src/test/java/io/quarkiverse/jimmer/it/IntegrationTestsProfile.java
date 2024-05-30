bc1082b92e1185fb3ba12bff3cdba8736573b3d9
flynndi <lixuan0520@gmail.com>
2024-05-30 08:20:25 +0800

    Integration-tests

--- integration-tests/src/test/java/io/quarkiverse/jimmer/it/IntegrationTestsProfile.java
+++ integration-tests/src/test/java/io/quarkiverse/jimmer/it/IntegrationTestsProfile.java
@@ -8,17 +8,17 @@ public class IntegrationTestsProfile implements QuarkusTestProfile {
 
     @Override
     public Map<String, String> getConfigOverrides() {
-        return Map.of(
-                "quarkus.datasource.db-kind", "h2",
-                "quarkus.datasource.username", "default",
-                "quarkus.datasource.jdbc.url", "jdbc:h2:mem:aaa",
-                "quarkus.datasource.jdbc.min-size", "2",
-                "quarkus.datasource.jdbc.max-size", "8",
-
-                "quarkus.datasource.DB2.db-kind", "h2",
-                "quarkus.datasource.DB2.username", "db2",
-                "quarkus.datasource.DB2.jdbc.url", "jdbc:h2:mem:bd2",
-                "quarkus.datasource.DB2.jdbc.min-size", "2",
-                "quarkus.datasource.DB2.jdbc.max-size", "8");
+        return Map.ofEntries(
+                Map.entry("quarkus.package.type", "uber-jar"),
+                Map.entry("quarkus.datasource.db-kind", "h2"),
+                Map.entry("quarkus.datasource.username", "default"),
+                Map.entry("quarkus.datasource.jdbc.url", "jdbc:h2:mem:aaa"),
+                Map.entry("quarkus.datasource.jdbc.min-size", "2"),
+                Map.entry("quarkus.datasource.jdbc.max-size", "8"),
+                Map.entry("quarkus.datasource.DB2.db-kind", "h2"),
+                Map.entry("quarkus.datasource.DB2.username", "db2"),
+                Map.entry("quarkus.datasource.DB2.jdbc.url", "jdbc:h2:mem:bd2"),
+                Map.entry("quarkus.datasource.DB2.jdbc.min-size", "2"),
+                Map.entry("quarkus.datasource.DB2.jdbc.max-size", "8"));
     }
 }
