336e33f130f7ae4ed8e5ad1db89b98571ceb862e
flynndi <lixuan0520@gmail.com>
2024-07-05 08:02:08 +0800

    Optimize

--- runtime/src/main/java/io/quarkiverse/jimmer/runtime/SqlClients.kt
+++ runtime/src/main/java/io/quarkiverse/jimmer/runtime/SqlClients.kt
@@ -1,8 +1,5 @@
 package io.quarkiverse.jimmer.runtime
 
-import io.quarkiverse.jimmer.runtime.cfg.JimmerBuildTimeConfig
-import io.quarkus.arc.ArcContainer
-import jakarta.enterprise.event.Event
 import org.babyfish.jimmer.sql.JSqlClient
 import org.babyfish.jimmer.sql.dialect.Dialect
 import org.babyfish.jimmer.sql.kt.KSqlClient
@@ -13,36 +10,25 @@ object SqlClients {
 
     @JvmStatic
     fun java(
-        config: JimmerBuildTimeConfig,
         dataSource: DataSource?,
         dataSourceName: String,
-        container: ArcContainer,
-        event: Event<Any>,
         dialect: Dialect
     ): JSqlClient =
-        JQuarkusSqlClient(config,
+        JQuarkusSqlClient(
             dataSource,
             dataSourceName,
-            container,
-            event,
             dialect,
             false)
 
     @JvmStatic
     fun kotlin(
-        config: JimmerBuildTimeConfig,
         dataSource: DataSource?,
         dataSourceName: String,
-        container: ArcContainer,
-        event: Event<Any>,
         dialect: Dialect
     ): KSqlClient =
         JQuarkusSqlClient(
-            config,
             dataSource,
             dataSourceName,
-            container,
-            event,
             dialect,
             true
         ).toKSqlClient()
