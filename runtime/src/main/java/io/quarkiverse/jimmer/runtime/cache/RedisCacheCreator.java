14c5361488f90bd9194a1c680dd7f4b984fd2db8
flynndi <lixuan0520@gmail.com>
2024-07-30 08:06:51 +0800

    RedisCacheCreator class is marked as deprecated

--- runtime/src/main/java/io/quarkiverse/jimmer/runtime/cache/RedisCacheCreator.java
+++ runtime/src/main/java/io/quarkiverse/jimmer/runtime/cache/RedisCacheCreator.java
@@ -19,6 +19,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import io.quarkus.redis.datasource.RedisDataSource;
 
+@Deprecated
 public class RedisCacheCreator extends AbstractCacheCreator {
 
     public RedisCacheCreator(RedisDataSource redisDataSource) {
