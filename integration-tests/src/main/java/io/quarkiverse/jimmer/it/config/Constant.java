6746d433da39a5b8bb3705241d3aab29c40f46de
flynndi <lixuan0520@gmail.com>
2024-08-25 11:16:13 +0800

    Optimize

---
+++ integration-tests/src/main/java/io/quarkiverse/jimmer/it/config/Constant.java
@@ -0,0 +1,12 @@
+package io.quarkiverse.jimmer.it.config;
+
+public class Constant {
+
+    public static final String DEFAULT = "<default>";
+
+    public static final String DATASOURCE2 = "DB2";
+
+    public static final String CONTAINER_DATASOURCE2 = "jimmer_container_DB2";
+
+    public static final String USER_ROLE_ID = "defc2d01-fb38-4d31-b006-fd182b25aa33";
+}
