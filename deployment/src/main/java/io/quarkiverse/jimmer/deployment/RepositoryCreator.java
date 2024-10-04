7238728a36805a2264e447293fd5c205677f5524
flynndi <lixuan0520@gmail.com>
2024-10-05 01:07:15 +0800

    Reconfiguration KRepository

--- deployment/src/main/java/io/quarkiverse/jimmer/deployment/RepositoryCreator.java
+++ deployment/src/main/java/io/quarkiverse/jimmer/deployment/RepositoryCreator.java
@@ -8,11 +8,14 @@ import java.util.Map;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import org.babyfish.jimmer.sql.JSqlClient;
+import org.babyfish.jimmer.sql.kt.KSqlClient;
 import org.jboss.jandex.DotName;
 import org.jboss.jandex.MethodInfo;
 
 import io.quarkiverse.jimmer.runtime.repository.JRepository;
 import io.quarkiverse.jimmer.runtime.repository.JRepositoryImpl;
+import io.quarkiverse.jimmer.runtime.repository.KRepository;
+import io.quarkiverse.jimmer.runtime.repository.KRepositoryImpl;
 import io.quarkus.agroal.DataSource;
 import io.quarkus.arc.Arc;
 import io.quarkus.arc.ArcContainer;
@@ -42,7 +45,7 @@ final class RepositoryCreator {
         this.dotIdDotNameEntry = dotIdDotNameEntry;
     }
 
-    Result implementCrudRepository() {
+    Result implementCrudJRepository() {
         DotName idTypeDotName = dotIdDotNameEntry.getKey();
         DotName entityDotName = dotIdDotNameEntry.getValue();
         String entityTypeStr = entityDotName.toString();
@@ -92,6 +95,78 @@ final class RepositoryCreator {
 
                 ResultHandle resultHandle = ctor.newInstance(
                         MethodDescriptor.ofConstructor(JRepositoryImpl.class, JSqlClient.class, Class.class),
+                        beanInstanceHandle, entityType);
+                ctor.writeInstanceField(delegateCreator.getFieldDescriptor(), ctor.getThis(), resultHandle);
+                ctor.returnValue(null);
+            }
+
+            for (MethodInfo methodInfo : methodInfos) {
+                try (MethodCreator ctor = classCreator.getMethodCreator(MethodDescriptor.of(methodInfo))) {
+                    ResultHandle delegate = ctor.readInstanceField(delegateCreator.getFieldDescriptor(),
+                            ctor.getThis());
+                    ResultHandle[] args = new ResultHandle[methodInfo.parametersCount()];
+                    for (int i = 0; i < methodInfo.parametersCount(); i++) {
+                        args[i] = ctor.getMethodParam(i);
+                    }
+                    ctor.returnValue(ctor.invokeInterfaceMethod(methodInfo, delegate, args));
+                }
+            }
+            classCreator.writeTo(classOutput);
+
+        }
+        return new Result(entityDotName, idTypeDotName, generatedClassName);
+    }
+
+    Result implementCrudKRepository() {
+        DotName idTypeDotName = dotIdDotNameEntry.getKey();
+        DotName entityDotName = dotIdDotNameEntry.getValue();
+        String entityTypeStr = entityDotName.toString();
+        String repositoryNameStr = repositoryName.toString();
+        String generatedClassName = repositoryNameStr + "_" + HashUtil.sha1(repositoryNameStr) + "Impl";
+
+        try (ClassCreator classCreator = ClassCreator.builder().classOutput(classOutput)
+                .className(generatedClassName)
+                .interfaces(repositoryNameStr)
+                .build()) {
+            classCreator.addAnnotation(ApplicationScoped.class);
+            classCreator.addAnnotation(Unremovable.class);
+            if (!dataSourceName.equals("<default>")) {
+                classCreator.addAnnotation(DataSource.class).add("value", dataSourceName);
+            }
+
+            FieldCreator delegateCreator = classCreator
+                    .getFieldCreator("delegate", KRepository.class.getName())
+                    .setModifiers(Modifier.PRIVATE | Modifier.FINAL);
+
+            try (MethodCreator ctor = classCreator.getMethodCreator("<init>", "V")) {
+                ctor.invokeSpecialMethod(MethodDescriptor.ofMethod(Object.class, "<init>", void.class), ctor.getThis());
+                ResultHandle entityType = ctor.loadClassFromTCCL(entityTypeStr);
+                ResultHandle containerHandle = ctor
+                        .invokeStaticMethod(MethodDescriptor.ofMethod(Arc.class, "container", ArcContainer.class));
+
+                ResultHandle instanceHandle;
+                if (!dataSourceName.equals("<default>")) {
+                    ResultHandle annotationArray = ctor.newArray(Annotation[].class, 1);
+                    ResultHandle arrayValue = ctor.newInstance(
+                            MethodDescriptor.ofConstructor(DataSource.DataSourceLiteral.class, String.class),
+                            ctor.load(dataSourceName));
+                    ctor.writeArrayValue(annotationArray, 0, arrayValue);
+                    instanceHandle = ctor.invokeInterfaceMethod(
+                            MethodDescriptor.ofMethod(ArcContainer.class, "instance", InstanceHandle.class, Class.class,
+                                    Annotation[].class),
+                            containerHandle, ctor.loadClass(KSqlClient.class), annotationArray);
+                } else {
+                    instanceHandle = ctor.invokeInterfaceMethod(
+                            MethodDescriptor.ofMethod(ArcContainer.class, "instance", InstanceHandle.class, Class.class,
+                                    Annotation[].class),
+                            containerHandle, ctor.loadClass(KSqlClient.class), ctor.loadNull());
+                }
+                ResultHandle beanInstanceHandle = ctor
+                        .invokeInterfaceMethod(MethodDescriptor.ofMethod(InstanceHandle.class, "get", Object.class),
+                                instanceHandle);
+
+                ResultHandle resultHandle = ctor.newInstance(
+                        MethodDescriptor.ofConstructor(KRepositoryImpl.class, KSqlClient.class, Class.class),
                         beanInstanceHandle, entityType);
                 ctor.writeInstanceField(delegateCreator.getFieldDescriptor(), ctor.getThis(), resultHandle);
                 ctor.returnValue(null);
