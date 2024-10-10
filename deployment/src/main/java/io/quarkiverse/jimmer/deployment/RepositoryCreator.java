44b9503a4d93f87275945f72e4a58469f19c8968
flynndi <lixuan0520@gmail.com>
2024-10-10 08:06:39 +0800

    Optimize

--- deployment/src/main/java/io/quarkiverse/jimmer/deployment/RepositoryCreator.java
+++ deployment/src/main/java/io/quarkiverse/jimmer/deployment/RepositoryCreator.java
@@ -21,6 +21,7 @@ import io.quarkus.arc.Arc;
 import io.quarkus.arc.ArcContainer;
 import io.quarkus.arc.InstanceHandle;
 import io.quarkus.arc.Unremovable;
+import io.quarkus.datasource.common.runtime.DataSourceUtil;
 import io.quarkus.gizmo.*;
 import io.quarkus.runtime.util.HashUtil;
 
@@ -58,7 +59,7 @@ final class RepositoryCreator {
                 .build()) {
             classCreator.addAnnotation(ApplicationScoped.class);
             classCreator.addAnnotation(Unremovable.class);
-            if (!dataSourceName.equals("<default>")) {
+            if (!dataSourceName.equals(DataSourceUtil.DEFAULT_DATASOURCE_NAME)) {
                 classCreator.addAnnotation(DataSource.class).add("value", dataSourceName);
             }
 
@@ -66,49 +67,50 @@ final class RepositoryCreator {
                     .getFieldCreator("delegate", JRepository.class.getName())
                     .setModifiers(Modifier.PRIVATE | Modifier.FINAL);
 
-            try (MethodCreator ctor = classCreator.getMethodCreator("<init>", "V")) {
-                ctor.invokeSpecialMethod(MethodDescriptor.ofMethod(Object.class, "<init>", void.class), ctor.getThis());
-                ResultHandle entityType = ctor.loadClassFromTCCL(entityTypeStr);
-                ResultHandle containerHandle = ctor
+            try (MethodCreator methodCreator = classCreator.getMethodCreator("<init>", "V")) {
+                methodCreator.invokeSpecialMethod(MethodDescriptor.ofMethod(Object.class, "<init>", void.class),
+                        methodCreator.getThis());
+                ResultHandle entityType = methodCreator.loadClassFromTCCL(entityTypeStr);
+                ResultHandle containerHandle = methodCreator
                         .invokeStaticMethod(MethodDescriptor.ofMethod(Arc.class, "container", ArcContainer.class));
 
                 ResultHandle instanceHandle;
-                if (!dataSourceName.equals("<default>")) {
-                    ResultHandle annotationArray = ctor.newArray(Annotation[].class, 1);
-                    ResultHandle arrayValue = ctor.newInstance(
+                if (!dataSourceName.equals(DataSourceUtil.DEFAULT_DATASOURCE_NAME)) {
+                    ResultHandle annotationArray = methodCreator.newArray(Annotation[].class, 1);
+                    ResultHandle arrayValue = methodCreator.newInstance(
                             MethodDescriptor.ofConstructor(DataSource.DataSourceLiteral.class, String.class),
-                            ctor.load(dataSourceName));
-                    ctor.writeArrayValue(annotationArray, 0, arrayValue);
-                    instanceHandle = ctor.invokeInterfaceMethod(
+                            methodCreator.load(dataSourceName));
+                    methodCreator.writeArrayValue(annotationArray, 0, arrayValue);
+                    instanceHandle = methodCreator.invokeInterfaceMethod(
                             MethodDescriptor.ofMethod(ArcContainer.class, "instance", InstanceHandle.class, Class.class,
                                     Annotation[].class),
-                            containerHandle, ctor.loadClass(JSqlClient.class), annotationArray);
+                            containerHandle, methodCreator.loadClass(JSqlClient.class), annotationArray);
                 } else {
-                    instanceHandle = ctor.invokeInterfaceMethod(
+                    instanceHandle = methodCreator.invokeInterfaceMethod(
                             MethodDescriptor.ofMethod(ArcContainer.class, "instance", InstanceHandle.class, Class.class,
                                     Annotation[].class),
-                            containerHandle, ctor.loadClass(JSqlClient.class), ctor.loadNull());
+                            containerHandle, methodCreator.loadClass(JSqlClient.class), methodCreator.loadNull());
                 }
-                ResultHandle beanInstanceHandle = ctor
+                ResultHandle beanInstanceHandle = methodCreator
                         .invokeInterfaceMethod(MethodDescriptor.ofMethod(InstanceHandle.class, "get", Object.class),
                                 instanceHandle);
 
-                ResultHandle resultHandle = ctor.newInstance(
+                ResultHandle resultHandle = methodCreator.newInstance(
                         MethodDescriptor.ofConstructor(JRepositoryImpl.class, JSqlClient.class, Class.class),
                         beanInstanceHandle, entityType);
-                ctor.writeInstanceField(delegateCreator.getFieldDescriptor(), ctor.getThis(), resultHandle);
-                ctor.returnValue(null);
+                methodCreator.writeInstanceField(delegateCreator.getFieldDescriptor(), methodCreator.getThis(), resultHandle);
+                methodCreator.returnValue(null);
             }
 
             for (MethodInfo methodInfo : methodInfos) {
-                try (MethodCreator ctor = classCreator.getMethodCreator(MethodDescriptor.of(methodInfo))) {
-                    ResultHandle delegate = ctor.readInstanceField(delegateCreator.getFieldDescriptor(),
-                            ctor.getThis());
+                try (MethodCreator methodCreator = classCreator.getMethodCreator(MethodDescriptor.of(methodInfo))) {
+                    ResultHandle delegate = methodCreator.readInstanceField(delegateCreator.getFieldDescriptor(),
+                            methodCreator.getThis());
                     ResultHandle[] args = new ResultHandle[methodInfo.parametersCount()];
                     for (int i = 0; i < methodInfo.parametersCount(); i++) {
-                        args[i] = ctor.getMethodParam(i);
+                        args[i] = methodCreator.getMethodParam(i);
                     }
-                    ctor.returnValue(ctor.invokeInterfaceMethod(methodInfo, delegate, args));
+                    methodCreator.returnValue(methodCreator.invokeInterfaceMethod(methodInfo, delegate, args));
                 }
             }
             classCreator.writeTo(classOutput);
@@ -130,7 +132,7 @@ final class RepositoryCreator {
                 .build()) {
             classCreator.addAnnotation(ApplicationScoped.class);
             classCreator.addAnnotation(Unremovable.class);
-            if (!dataSourceName.equals("<default>")) {
+            if (!dataSourceName.equals(DataSourceUtil.DEFAULT_DATASOURCE_NAME)) {
                 classCreator.addAnnotation(DataSource.class).add("value", dataSourceName);
             }
 
@@ -138,49 +140,50 @@ final class RepositoryCreator {
                     .getFieldCreator("delegate", KRepository.class.getName())
                     .setModifiers(Modifier.PRIVATE | Modifier.FINAL);
 
-            try (MethodCreator ctor = classCreator.getMethodCreator("<init>", "V")) {
-                ctor.invokeSpecialMethod(MethodDescriptor.ofMethod(Object.class, "<init>", void.class), ctor.getThis());
-                ResultHandle entityType = ctor.loadClassFromTCCL(entityTypeStr);
-                ResultHandle containerHandle = ctor
+            try (MethodCreator methodCreator = classCreator.getMethodCreator("<init>", "V")) {
+                methodCreator.invokeSpecialMethod(MethodDescriptor.ofMethod(Object.class, "<init>", void.class),
+                        methodCreator.getThis());
+                ResultHandle entityType = methodCreator.loadClassFromTCCL(entityTypeStr);
+                ResultHandle containerHandle = methodCreator
                         .invokeStaticMethod(MethodDescriptor.ofMethod(Arc.class, "container", ArcContainer.class));
 
                 ResultHandle instanceHandle;
-                if (!dataSourceName.equals("<default>")) {
-                    ResultHandle annotationArray = ctor.newArray(Annotation[].class, 1);
-                    ResultHandle arrayValue = ctor.newInstance(
+                if (!dataSourceName.equals(DataSourceUtil.DEFAULT_DATASOURCE_NAME)) {
+                    ResultHandle annotationArray = methodCreator.newArray(Annotation[].class, 1);
+                    ResultHandle arrayValue = methodCreator.newInstance(
                             MethodDescriptor.ofConstructor(DataSource.DataSourceLiteral.class, String.class),
-                            ctor.load(dataSourceName));
-                    ctor.writeArrayValue(annotationArray, 0, arrayValue);
-                    instanceHandle = ctor.invokeInterfaceMethod(
+                            methodCreator.load(dataSourceName));
+                    methodCreator.writeArrayValue(annotationArray, 0, arrayValue);
+                    instanceHandle = methodCreator.invokeInterfaceMethod(
                             MethodDescriptor.ofMethod(ArcContainer.class, "instance", InstanceHandle.class, Class.class,
                                     Annotation[].class),
-                            containerHandle, ctor.loadClass(KSqlClient.class), annotationArray);
+                            containerHandle, methodCreator.loadClass(KSqlClient.class), annotationArray);
                 } else {
-                    instanceHandle = ctor.invokeInterfaceMethod(
+                    instanceHandle = methodCreator.invokeInterfaceMethod(
                             MethodDescriptor.ofMethod(ArcContainer.class, "instance", InstanceHandle.class, Class.class,
                                     Annotation[].class),
-                            containerHandle, ctor.loadClass(KSqlClient.class), ctor.loadNull());
+                            containerHandle, methodCreator.loadClass(KSqlClient.class), methodCreator.loadNull());
                 }
-                ResultHandle beanInstanceHandle = ctor
+                ResultHandle beanInstanceHandle = methodCreator
                         .invokeInterfaceMethod(MethodDescriptor.ofMethod(InstanceHandle.class, "get", Object.class),
                                 instanceHandle);
 
-                ResultHandle resultHandle = ctor.newInstance(
+                ResultHandle resultHandle = methodCreator.newInstance(
                         MethodDescriptor.ofConstructor(KRepositoryImpl.class, KSqlClient.class, Class.class),
                         beanInstanceHandle, entityType);
-                ctor.writeInstanceField(delegateCreator.getFieldDescriptor(), ctor.getThis(), resultHandle);
-                ctor.returnValue(null);
+                methodCreator.writeInstanceField(delegateCreator.getFieldDescriptor(), methodCreator.getThis(), resultHandle);
+                methodCreator.returnValue(null);
             }
 
             for (MethodInfo methodInfo : methodInfos) {
-                try (MethodCreator ctor = classCreator.getMethodCreator(MethodDescriptor.of(methodInfo))) {
-                    ResultHandle delegate = ctor.readInstanceField(delegateCreator.getFieldDescriptor(),
-                            ctor.getThis());
+                try (MethodCreator methodCreator = classCreator.getMethodCreator(MethodDescriptor.of(methodInfo))) {
+                    ResultHandle delegate = methodCreator.readInstanceField(delegateCreator.getFieldDescriptor(),
+                            methodCreator.getThis());
                     ResultHandle[] args = new ResultHandle[methodInfo.parametersCount()];
                     for (int i = 0; i < methodInfo.parametersCount(); i++) {
-                        args[i] = ctor.getMethodParam(i);
+                        args[i] = methodCreator.getMethodParam(i);
                     }
-                    ctor.returnValue(ctor.invokeInterfaceMethod(methodInfo, delegate, args));
+                    methodCreator.returnValue(methodCreator.invokeInterfaceMethod(methodInfo, delegate, args));
                 }
             }
             classCreator.writeTo(classOutput);
