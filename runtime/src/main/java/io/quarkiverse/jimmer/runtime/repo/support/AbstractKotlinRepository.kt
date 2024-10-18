b06e02d2a422ddaf868d41ee9c03be635cc7f8b1
flynndi <lixuan0520@gmail.com>
2024-10-19 00:06:04 +0800

    Methods change

--- runtime/src/main/java/io/quarkiverse/jimmer/runtime/repo/support/AbstractKotlinRepository.kt
+++ runtime/src/main/java/io/quarkiverse/jimmer/runtime/repo/support/AbstractKotlinRepository.kt
@@ -28,8 +28,7 @@ abstract class AbstractKotlinRepository<E: Any, ID: Any>(protected val sqlClient
     @Suppress("UNCHECKED_CAST")
     protected val entityType: KClass<E> = JpaOperationsData.getEntityClass(this.javaClass).let { it.kotlin as KClass<E> }
 
-    protected val immutableType: ImmutableType =
-        ImmutableType.get(this.entityType.java)
+    protected val immutableType: ImmutableType = ImmutableType.get(this.entityType.java)
 
     override fun findById(id: ID, fetcher: Fetcher<E>?): E? =
         if (fetcher == null) {
@@ -121,17 +120,13 @@ abstract class AbstractKotlinRepository<E: Any, ID: Any>(protected val sqlClient
             select(table.fetch(viewType))
         }.fetchSlice(limit, offset)
 
-    override fun saveEntity(entity: E, block: (KSaveCommandDsl.() -> Unit)?): KSimpleSaveResult<E> =
-        if (block == null) {
-            sqlClient.save(entity)
-        } else {
-            sqlClient.save(entity, block)
-        }
+    override fun save(entity: E, block: (KSaveCommandDsl.() -> Unit)?): KSimpleSaveResult<E> =
+        sqlClient.entities.save(entity, null, block)
 
     override fun saveEntities(entities: Collection<E>, block: (KSaveCommandDsl.() -> Unit)?): KBatchSaveResult<E> =
         sqlClient.entities.saveEntities(entities, null, block)
 
-    override fun saveInput(input: Input<E>, block: (KSaveCommandDsl.() -> Unit)?): KSimpleSaveResult<E> =
+    override fun save(input: Input<E>, block: (KSaveCommandDsl.() -> Unit)?): KSimpleSaveResult<E> =
         sqlClient.entities.save(input, null, block)
 
     override fun saveInputs(inputs: Collection<Input<E>>, block: (KSaveCommandDsl.() -> Unit)?): KBatchSaveResult<E> =
