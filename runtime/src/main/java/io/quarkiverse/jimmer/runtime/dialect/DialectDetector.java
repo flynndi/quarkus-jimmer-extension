f024a8c6d3388d45c0726715e8e134894524d7f7
flynndi <lixuan0520@gmail.com>
2024-12-08 11:30:04 +0800

    Default implements DialectDetector

--- runtime/src/main/java/io/quarkiverse/jimmer/runtime/dialect/DialectDetector.java
+++ runtime/src/main/java/io/quarkiverse/jimmer/runtime/dialect/DialectDetector.java
@@ -1,6 +1,10 @@
 package io.quarkiverse.jimmer.runtime.dialect;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
 
 import org.babyfish.jimmer.sql.dialect.Dialect;
 import org.jboss.logging.Logger;
@@ -16,8 +20,20 @@ public interface DialectDetector {
 
         private static final Logger log = Logger.getLogger(DialectDetector.class);
 
+        private final DataSource dataSource;
+
+        public Impl(DataSource dataSource) {
+            this.dataSource = dataSource;
+        }
+
         @Override
         public @Nullable Dialect detectDialect(@NotNull Connection con) {
+            try (Connection connection = dataSource.getConnection()) {
+                DatabaseMetaData metaData = connection.getMetaData();
+                String databaseProductName = metaData.getDatabaseProductName();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
             return null;
         }
     }
