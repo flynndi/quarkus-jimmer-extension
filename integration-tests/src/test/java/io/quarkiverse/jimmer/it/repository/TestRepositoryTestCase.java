2f5540d697790f4bb4f4d9d11da43269a165396b
flynndi <lixuan0520@gmail.com>
2025-01-28 13:17:42 +0800

    Integration-tests

--- integration-tests/src/test/java/io/quarkiverse/jimmer/it/repository/TestRepositoryTestCase.java
+++ integration-tests/src/test/java/io/quarkiverse/jimmer/it/repository/TestRepositoryTestCase.java
@@ -5,6 +5,8 @@ import jakarta.inject.Inject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import io.quarkiverse.jimmer.it.Constant;
+import io.quarkus.agroal.DataSource;
 import io.quarkus.arc.Arc;
 import io.quarkus.test.junit.QuarkusTest;
 
@@ -14,9 +16,16 @@ public class TestRepositoryTestCase {
     @Inject
     BookRepository bookRepository;
 
+    @Inject
+    @DataSource(Constant.DATASOURCE2)
+    UserRoleRepository userRoleRepository;
+
     @Test
     void testRepositoryBean() {
         BookRepository bookRepositoryFromArc = Arc.container().instance(BookRepository.class).get();
         Assertions.assertEquals(bookRepository, bookRepositoryFromArc);
+        UserRoleRepository userRoleRepositoryFromArc = Arc.container()
+                .instance(UserRoleRepository.class, new DataSource.DataSourceLiteral(Constant.DATASOURCE2)).get();
+        Assertions.assertEquals(userRoleRepository, userRoleRepositoryFromArc);
     }
 }
