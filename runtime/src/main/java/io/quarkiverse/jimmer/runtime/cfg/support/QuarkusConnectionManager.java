091415d81c5910a5af16cfedf65516c7c53c25b0
flynndi <lixuan0520@gmail.com>
2025-03-11 08:30:32 +0800

    jimmer 0.9.62 -> 0.9.63

--- runtime/src/main/java/io/quarkiverse/jimmer/runtime/cfg/support/QuarkusConnectionManager.java
+++ runtime/src/main/java/io/quarkiverse/jimmer/runtime/cfg/support/QuarkusConnectionManager.java
@@ -3,18 +3,38 @@ package io.quarkiverse.jimmer.runtime.cfg.support;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import javax.sql.DataSource;
 
+import jakarta.transaction.TransactionManager;
+
 import org.babyfish.jimmer.sql.runtime.ConnectionManager;
+import org.babyfish.jimmer.sql.transaction.Propagation;
+import org.babyfish.jimmer.sql.transaction.TxConnectionManager;
 import org.jetbrains.annotations.Nullable;
 
-public class QuarkusConnectionManager implements ConnectionManager {
+public class QuarkusConnectionManager implements ConnectionManager, TxConnectionManager {
 
     private final DataSource dataSource;
 
+    private final Supplier<TransactionManager> transactionManagerResolver;
+
+    private volatile Object transactionManagerOrException;
+
     public QuarkusConnectionManager(DataSource dataSource) {
         this.dataSource = dataSource;
+        this.transactionManagerResolver = null;
+    }
+
+    public QuarkusConnectionManager(DataSource dataSource, Supplier<TransactionManager> transactionManagerResolver) {
+        this.dataSource = dataSource;
+        this.transactionManagerResolver = transactionManagerResolver;
+    }
+
+    @Override
+    public final <R> R execute(Function<Connection, R> block) {
+        return execute(null, block);
     }
 
     @Override
@@ -27,5 +47,10 @@ public class QuarkusConnectionManager implements ConnectionManager {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public <R> R executeTransaction(Propagation propagation, Function<Connection, R> block) {
+        throw new UnsupportedOperationException("Quarkus does not support Jimmer's transaction management");
     }
 }
