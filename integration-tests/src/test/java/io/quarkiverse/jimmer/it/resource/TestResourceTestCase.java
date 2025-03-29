1654712a5db58833c372e1875e2ba89662dacbe9
flynndi <lixuan0520@gmail.com>
2025-03-29 14:25:53 +0800

    Optimize

--- integration-tests/src/test/java/io/quarkiverse/jimmer/it/resource/TestResourceTestCase.java
+++ integration-tests/src/test/java/io/quarkiverse/jimmer/it/resource/TestResourceTestCase.java
@@ -935,12 +935,13 @@ public class TestResourceTestCase {
 
     @Test
     void testBookRepositoryFindAllPageSortView() {
-        String body = """
-                {
-                    "index": 0,
-                    "size": 1
-                }
-                """;
+        String body;
+        Pagination pagination = new Pagination(0, 1);
+        try {
+            body = objectMapper.writeValueAsString(pagination);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         Response response = given()
                 .header(new Header(HttpHeaders.CONTENT_TYPE.toString(), HttpHeaderValues.APPLICATION_JSON.toString()))
                 .body(body)
