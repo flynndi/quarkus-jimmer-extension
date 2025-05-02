77f90225f78034244b65d626111ca498eeb71bc6
flynndi <lixuan0520@gmail.com>
2025-05-03 00:02:28 +0800

    Distinguish the configuration between build and runtime

--- runtime/src/main/java/io/quarkiverse/jimmer/runtime/cfg/JimmerRuntimeConfig.java
+++ runtime/src/main/java/io/quarkiverse/jimmer/runtime/cfg/JimmerRuntimeConfig.java
@@ -2,10 +2,7 @@ package io.quarkiverse.jimmer.runtime.cfg;
 
 import java.util.*;
 
-import org.babyfish.jimmer.sql.EnumType;
-import org.babyfish.jimmer.sql.fetcher.ReferenceFetchType;
 import org.babyfish.jimmer.sql.runtime.DatabaseValidationMode;
-import org.babyfish.jimmer.sql.runtime.IdOnlyTargetCheckingLevel;
 
 import io.quarkus.datasource.common.runtime.DataSourceUtil;
 import io.quarkus.runtime.annotations.ConfigDocMapKey;
@@ -28,16 +25,10 @@ public interface JimmerRuntimeConfig {
     Map<String, JimmerDataSourceRuntimeConfig> dataSources();
 
     /**
-     * jimmer.defaultReferenceFetchType
+     * jimmer.transactionCacheOperatorFixedDelay
      */
-    @WithDefault("SELECT")
-    ReferenceFetchType defaultReferenceFetchType();
-
-    /**
-     * jimmer.maxJoinFetchDepth
-     */
-    @WithDefault("3")
-    OptionalInt maxJoinFetchDepth();
+    @WithDefault("5s")
+    String transactionCacheOperatorFixedDelay();
 
     /**
      * jimmer.databaseValidationMode
@@ -50,107 +41,6 @@ public interface JimmerRuntimeConfig {
      */
     @Deprecated
     DatabaseValidation databaseValidation();
-
-    /**
-     * jimmer.defaultDissociationActionCheckable
-     */
-    @WithDefault("true")
-    boolean defaultDissociationActionCheckable();
-
-    /**
-     * jimmer.idOnlyTargetCheckingLevel
-     */
-    @WithDefault("NONE")
-    IdOnlyTargetCheckingLevel idOnlyTargetCheckingLevel();
-
-    /**
-     * jimmer.transactionCacheOperatorFixedDelay
-     */
-    @WithDefault("5s")
-    String transactionCacheOperatorFixedDelay();
-
-    /**
-     * jimmer.defaultEnumStrategy
-     */
-    @WithDefault("NAME")
-    EnumType.Strategy defaultEnumStrategy();
-
-    /**
-     * jimmer.defaultBatchSize
-     */
-    @WithDefault("128")
-    OptionalInt defaultBatchSize();
-
-    /**
-     * jimmer.inListPaddingEnabled
-     */
-    @WithDefault("false")
-    boolean inListPaddingEnabled();
-
-    /**
-     * jimmer.expandedInListPaddingEnabled
-     */
-    @WithDefault("false")
-    boolean expandedInListPaddingEnabled();
-
-    /**
-     * jimmer.defaultListBatchSize
-     */
-    @WithDefault("16")
-    OptionalInt defaultListBatchSize();
-
-    /**
-     * jimmer.offsetOptimizingThreshold
-     */
-    @WithDefault("2147483647")
-    OptionalInt offsetOptimizingThreshold();
-
-    /**
-     * jimmer.isForeignKeyEnabledByDefault
-     */
-    @WithDefault("true")
-    boolean isForeignKeyEnabledByDefault();
-
-    /**
-     * jimmer.maxCommandJoinCount
-     */
-    @WithDefault("2")
-    int maxCommandJoinCount();
-
-    /**
-     * jimmer.mutationTransactionRequired
-     */
-    @WithDefault("false")
-    boolean mutationTransactionRequired();
-
-    /**
-     * jimmer.targetTransferable
-     */
-    @WithDefault("false")
-    boolean targetTransferable();
-
-    /**
-     * jimmer.explicitBatchEnabled
-     */
-    @WithDefault("false")
-    boolean explicitBatchEnabled();
-
-    /**
-     * jimmer.dumbBatchAcceptable
-     */
-    @WithDefault("false")
-    boolean dumbBatchAcceptable();
-
-    /**
-     * jimmer.constraintViolationTranslatable
-     */
-    @WithDefault("true")
-    boolean constraintViolationTranslatable();
-
-    /**
-     * jimmer.executorContextPrefixes
-     */
-    Optional<List<String>> executorContextPrefixes();
 
     @Deprecated
     @ConfigGroup
