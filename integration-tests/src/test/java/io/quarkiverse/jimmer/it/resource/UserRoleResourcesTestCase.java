a06b5e13633c85a3cde7b6d2f92683682f3b6f51
flynndi <lixuan0520@gmail.com>
2025-05-30 08:12:15 +0800

    Integration-tests

--- integration-tests/src/test/java/io/quarkiverse/jimmer/it/resource/UserRoleResourcesTestCase.java
+++ integration-tests/src/test/java/io/quarkiverse/jimmer/it/resource/UserRoleResourcesTestCase.java
@@ -46,5 +46,6 @@ public class UserRoleResourcesTestCase {
                 .when()
                 .get("userRoleResources/testUserRoleSpecification");
         Assertions.assertEquals(HttpStatus.SC_OK, response.statusCode());
+        Assertions.assertEquals(Constant.USER_ID, response.jsonPath().getString("[0].userId"));
     }
 }
