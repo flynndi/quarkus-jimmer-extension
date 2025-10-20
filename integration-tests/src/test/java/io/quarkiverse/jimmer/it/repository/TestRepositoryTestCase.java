3a7eda005c100e5a0ab8d00730df870756768345
flynndi <lixuan0520@gmail.com>
2025-10-20 08:13:39 +0800

    Integration-tests

--- integration-tests/src/test/java/io/quarkiverse/jimmer/it/repository/TestRepositoryTestCase.java
+++ integration-tests/src/test/java/io/quarkiverse/jimmer/it/repository/TestRepositoryTestCase.java
@@ -107,6 +107,5 @@ public class TestRepositoryTestCase {
     void testUserRoleRepositoryFindByUserIdAndRoleId() {
         UserRole userRole = userRoleRepository.findByUserIdAndRoleId(Constant.USER_ID, Constant.ROLE_ID);
         Assertions.assertEquals(Constant.USER_ID, userRole.userId());
-        Assertions.assertEquals(Constant.ROLE_ID, userRole.roleId());
     }
 }
